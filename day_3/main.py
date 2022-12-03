af981645f4bbb0c28a7c5e9342cae8f73ed76540
Paavo Rotsten <paavo.rotsten@aalto.fi>
2022-12-03 19:38:50 +0200

    Improved code quality for day 3.

--- day_3/main.py
+++ day_3/main.py
@@ -2,7 +2,7 @@ import string
 
 def input_lines():
 	with open("./input.txt", "r") as file:
-		return_value = file.readlines()
+		return_value = file.read().splitlines()
 	return return_value
 
 priority_map = {
@@ -14,51 +14,30 @@ print(priority_map)
 def priority(char):
 	return priority_map[char]
 
-def to_comps(line):
-	# get compartments
-	first_comp = line[:len(line)//2]
-	second_comp = line[len(line)//2:]
-	return first_comp, second_comp
-
-def get_common_item(first_comp, second_comp):
-	# get items in both compartments
-	both_comps_items = set.intersection(\
-		set(first_comp),\
-		set(second_comp))
-
-	# there should be only 1 common item.
-	assert len(both_comps_items), 1
-	common_item = list(both_comps_items)[0]
-	return common_item
-
 def main():
 	priority_sum = 0
 	badge_sum = 0
+	input_list = input_lines() # reads file
 
-	possible_group_characters = set(' ')
-	for i, line in enumerate(input_lines() + ["  "]):
-		if i % 3 == 0: # end last group, then start new one.
-			assert len(possible_group_characters), 1
-			group_badge = list(possible_group_characters)[0]
-			badge_sum += priority(group_badge)
-			print(group_badge, priority(group_badge))
-			# reset
-			possible_group_characters = set(string.ascii_letters)
-		print(line)
-		# solve second problem
-		possible_group_characters = set.intersection( \
-			possible_group_characters, \
-			set(line)
-		)
+	# splits input_list into groups of 3
+	groups = [input_list[x:x+3] for x in range(0, len(input_list), 3)]
 
-		# solve first problem
-		first_comp, second_comp = to_comps(line)
-		common_item = get_common_item(first_comp, second_comp)
-		char_priority = priority(common_item)
+	# do for every group (solve part 2)
+	for group in groups:
+		# get badge
+		badge = set.intersection(*[set(s) for s in group]).pop()
+		badge_sum += priority(badge)
 
-		priority_sum += char_priority
-	print(priority_sum)
-	print(badge_sum)
+		# do for every line (solve part 1)
+		for line in group:
+			# get compartments
+			first_comp  = line[:len(line)//2]
+			second_comp = line[len(line)//2:]
+
+			common_item = set.intersection(set(first_comp), set(second_comp)).pop()
+			priority_sum += priority(common_item)
+
+	print(priority_sum, badge_sum)
 
 if __name__ == "__main__":
 	main()

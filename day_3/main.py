eb9f73163df0fcb220b288c9b8e65d91a0c1e5d5
Paavo Rotsten <paavo.rotsten@aalto.fi>
2022-12-03 19:29:04 +0200

    Made an ugly solution for part 2 of day 3.

--- day_3/main.py
+++ day_3/main.py
@@ -1,12 +1,15 @@
+import string
+
 def input_lines():
 	with open("./input.txt", "r") as file:
 		return_value = file.readlines()
 	return return_value
 
-
-_lowercase = dict(zip("abcdefghijklmnopqrstuvwxyz", range(1, 26 +1)))
-_uppercase = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ", range(27, 52 +1)))
-priority_map = {**_lowercase, **_uppercase}
+priority_map = {
+	**dict(zip(string.ascii_letters, range(1, 52 +1))),\
+	' ': 0	
+}
+print(priority_map)
 
 def priority(char):
 	return priority_map[char]
@@ -30,13 +33,32 @@ def get_common_item(first_comp, second_comp):
 
 def main():
 	priority_sum = 0
-	for line in input_lines():
+	badge_sum = 0
+
+	possible_group_characters = set(' ')
+	for i, line in enumerate(input_lines() + ["  "]):
+		if i % 3 == 0: # end last group, then start new one.
+			assert len(possible_group_characters), 1
+			group_badge = list(possible_group_characters)[0]
+			badge_sum += priority(group_badge)
+			print(group_badge, priority(group_badge))
+			# reset
+			possible_group_characters = set(string.ascii_letters)
+		print(line)
+		# solve second problem
+		possible_group_characters = set.intersection( \
+			possible_group_characters, \
+			set(line)
+		)
+
+		# solve first problem
 		first_comp, second_comp = to_comps(line)
 		common_item = get_common_item(first_comp, second_comp)
 		char_priority = priority(common_item)
 
 		priority_sum += char_priority
 	print(priority_sum)
+	print(badge_sum)
 
 if __name__ == "__main__":
 	main()

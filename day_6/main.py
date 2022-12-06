c1bf4f32e2977357ff5bed362a2c59e36ac8ccbb
Paavo Rotsten <paavo.rotsten@aalto.fi>
2022-12-06 21:04:29 +0200

    Added day 6 part 2 solution.

--- day_6/main.py
+++ day_6/main.py
@@ -3,20 +3,25 @@ def input_lines():
 		return_value = file.readlines()
 	return return_value
 
-# sop=start of packet
-def find_sop_marker(string, slice_length=4):
-
-
+def find_marker(string, *, slice_length):
+	# divide a string into all its [slice_length] -length substrings
 	slices = [string[index:index+slice_length] for index in range(0, len(string)-slice_length)]
+
+	# go through all slices in order and find the first one where all character are unique.
 	for index, slice in enumerate(slices):
-		# length of (list) set 
-		if len(set(slice)) == slice_length:
-			return index+slice_length
+		# if all elements are unique
+		if len(set(slice)) == len(slice):
+			return index+slice_length # add slice length because we want end of the marker
 
 
 def main():
-	sop_marker = find_sop_marker(input_lines()[0])
+	# part 1
+	sop_marker = find_marker(input_lines()[0], slice_length=4)
 	print(sop_marker)
+
+	# part 2
+	message_marker = find_marker(input_lines()[0], slice_length=14)
+	print(message_marker)
 
 if __name__ == "__main__":
 	main()
